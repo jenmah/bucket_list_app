093a0d65a740f112fc70e1e2c18b60db336d4405
Jennifer Mah <jen.mah@hotmail.com>
2015-07-10 13:17:50 +0100

    added more template code

--- server/app/assets/javascripts/views/user_menu.js
+++ server/app/assets/javascripts/views/user_menu.js
@@ -3,7 +3,6 @@ bucketlistApp.Views.UserMenuView = Backbone.View.extend({
 
  	console.log('user menu render function called');
  	var user = new bucketlistApp.Models.UserSession(); 
- 	debugger;
  	var template = _.template($('#user-menu-template').html());
  	console.log(template);
   this.$el.html(template({user: user}));
