05d48372b3cbbbc5df6dbd0957c6b9b9e2b69b0d
Jennifer Mah <jen.mah@hotmail.com>
2015-07-10 19:21:08 +0100

    client side code '

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,11 @@
 bucketlistApp.bucketlist = new bucketlistApp.Bucketlist([]);
 
 $(document).ready(function(){
+	_.templateSettings = {
+    evaluate:    /\{\{#([\s\S]+?)\}\}/g,
+    interpolate: /\{\{[^#\{]([\s\S]+?)[^\}]\}\}/g,
+    escape:      /\{\{\{([\s\S]+?)\}\}\}/g,
+  }
 	// Creating a new router instance
 	var router = new bucketlistApp.AppRouter();
 	Backbone.history.start();
